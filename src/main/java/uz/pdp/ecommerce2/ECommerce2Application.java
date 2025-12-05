135adbca7a6fd95b7e5c6675b12066e83339cddb
gammer <145098169+akbarAhmatov@users.noreply.github.com>
2025-12-05 22:16:29 +0500

    1

---
+++ src/main/java/uz/pdp/ecommerce2/ECommerce2Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.ecommerce2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerce2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerce2Application.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/ecommerce2/ECommerce2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.ecommerce2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerce2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
